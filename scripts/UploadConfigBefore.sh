29361427fc37504b2fdf5b884e283f6c61981a70
BRSweagle <46354067+BRSweagle@users.noreply.github.com>
2019-04-12 10:11:23 +0100

    Update UploadConfigBefore.sh

--- scripts/UploadConfigBefore.sh
+++ scripts/UploadConfigBefore.sh
@@ -2,9 +2,7 @@
 source $(dirname "$0")/sweagle.env
 
 env=$1
-nodePathUI="webportal1,components,ui,versions,ui-1.1"
-nodePathEnv="webportal1,environments,$env,assigned,properties"
-nodePathHelm="webportal1,environments,$env,assigned,helm,nginx-ingress"
+nodePathUI="JenkindsCD"
 
 $sweagleScriptDir/webPortalUploadConfig.sh $nodePathUI
 $sweagleScriptDir/uploadFileToSweagle.sh $nodePathEnv ./environments/$env.properties
