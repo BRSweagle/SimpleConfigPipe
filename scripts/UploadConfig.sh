f2c03adb280c54559be798872b67e87ca79b4f72
BRSweagle <46354067+BRSweagle@users.noreply.github.com>
2019-04-12 11:41:53 +0100

    Update UploadConfig.sh

--- scripts/UploadConfig.sh
+++ scripts/UploadConfig.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 source $(dirname "$0")/sweagle.env
 
-argFileCSS="./public/css/styleMI6.css"
-argFileJSON="./public/css/styleMI6.json"
-argDirJSON="./public/json"
-argColor="background-color"
+argFileJSON="./config/Envs.json"
 # Sweagle path to store css, it is defined by each of the node names separated by ,
 nodePath=$1
 
