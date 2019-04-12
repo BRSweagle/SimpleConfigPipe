e69a27e1a93d5ce917f2fdda0dd327a997d1f248
BRSweagle <46354067+BRSweagle@users.noreply.github.com>
2019-04-12 13:05:59 +0100

    Update UploadConfigBefore.sh

--- scripts/UploadConfigBefore.sh
+++ scripts/UploadConfigBefore.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 source $(dirname "$0")/sweagle.env
 
-env=$1
-nodePathUI="JenkinsCD"
+#env=$1
+nodePathEnv="JenkinsCD"
+file="./config/Envs.json"
 
-$sweagleScriptDir/UploadConfig.sh $nodePathUI
-$sweagleScriptDir/uploadFileToSweagle.sh $nodePathEnv
-
-# Upload all helm yaml configuration, if any for this environment
-if [ -d "./environments/$env/helm/nginx-ingress" ]; then
-  $sweagleScriptDir/uploadDirToSweagle.sh $nodePathHelm ./environments/$env/helm/nginx-ingress yaml
-fi
+#$sweagleScriptDir/UploadConfig.sh $nodePathUI
+$sweagleScriptDir/uploadFileToSweagle.sh $nodePathEnv $file
